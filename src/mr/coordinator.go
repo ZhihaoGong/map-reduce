8293d7790825a0a82549d1da6318e7cb1df711d5
zhihao-gong <zhihao.gong@tusimple.ai>
2021-05-09 21:31:31 +0800

    Revert back coordinator

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,13 +1,10 @@
 package mr
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/rpc"
-	"sync"
-	"time"
 )
 
 //
@@ -34,12 +31,6 @@ func (c *Coordinator) ApplyForTask(request *TaskApplyReq, reply *TaskApplyRes) e
 	return nil
 }
 
-func (c *Coordinator) getNextWorkerID() int {
-	// may overflow
-	c.nextWorkerID++
-	return c.nextWorkerID
-}
-
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -48,55 +39,6 @@ func (c *Coordinator) server() {
 		log.Fatal("listen error:", e)
 	}
 	go http.Serve(l, nil)
-}
-
-//
-// HeartBeat
-//
-func (c *Coordinator) heartBeatCheck() {
-	go func() {
-		for {
-			expireWorkers := []int{}
-			c.workerColLock.Lock()
-			for workerID, worker := range c.workerCol {
-				if worker.lastHeartBeart+10 < time.Now().Unix() {
-					fmt.Println("eeeee", workerID)
-					expireWorkers = append(expireWorkers, workerID)
-					fmt.Println("eeeee", expireWorkers)
-				}
-			}
-			for _, workerID := range expireWorkers {
-				fmt.Println("eeeee", expireWorkers)
-				delete(c.workerCol, workerID)
-			}
-
-			fmt.Println("heartbeatcheck", c.workerCol, int(time.Now().Unix()))
-			c.workerColLock.Unlock()
-			time.Sleep(time.Second)
-		}
-	}()
-}
-
-func (c *Coordinator) renewHeartBeat(workerID int, reply *TaskApplyRes) {
-	c.workerColLock.Lock()
-	if workerID == -1 {
-		// a new worker
-		workerID = c.getNextWorkerID()
-		reply.WorkerID = workerID
-		c.workerCol[workerID] = &WorkerInfo{
-			id:             workerID,
-			status:         IdleWorker,
-			lastHeartBeart: time.Now().Unix(),
-		}
-	}
-
-	worker := c.workerCol[workerID]
-	worker.lastHeartBeart = time.Now().Unix()
-	fmt.Println("22222")
-	for k, v := range c.workerCol {
-		fmt.Println(k, *v)
-	}
-	c.workerColLock.Unlock()
 }
 
 //
@@ -120,43 +62,6 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	}
 
 	// Your code here.
-	// initalization
-	c.workerCol = make(map[int]*WorkerInfo)
-
-	// c.InitedTasks = make(map[int]*TaskInfo)
-	// c.MappingTasks = make(map[int]*TaskInfo)
-	// c.MappedTasks = make(map[int]*TaskInfo)
-	// c.ReducingTasks = make(map[int]*TaskInfo)
-	// c.ReducedTasks = make(map[int]*TaskInfo)
-
-	c.workerColLock = sync.Mutex{}
-
-	c.mapTasks = make(map[int]*TaskInfo)
-	c.reduceTasks = make(map[int]*TaskInfo)
-
-	// create map tasks
-	for index, filename := range files {
-		c.mapTasks[index] = &TaskInfo{
-			Id:            index,
-			FileLocations: []string{filename},
-			Status:        Pending,
-			TaskType:      Map,
-		}
-	}
-
-	// create reduce tasks
-	for i := 1; i < nReduce; i++ {
-		c.reduceTasks[i] = &TaskInfo{
-			Id:            i,
-			FileLocations: []string{},
-			Status:        Pending,
-			TaskType:      Reduce,
-		}
-	}
-
-	c.heartBeatCheck()
-
-	fmt.Println("111111111111", c.mapTasks, c.reduceTasks)
 
 	c.server()
 	return &c
